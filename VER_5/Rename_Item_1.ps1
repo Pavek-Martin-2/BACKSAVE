996cf8782d4996f0a980e5ea5f51b5278971387f
Martin Pávek <pavek_martin@seznam.cz>
2026-08-14 09:56:47 +0200

    Add files via upload

---
+++ VER_5/Rename_Item_1.ps1
@@ -0,0 +1,14 @@
+﻿cls
+
+# utilita na prejmenovani screenshotu, vytvorene klavesou F12 ve hre "NAM" 
+
+Remove-Variable files, item -ErrorAction SilentlyContinue
+$files += @( Get-ChildItem -Include "NAM0????" -Name )
+
+foreach ($item in $files) {
+echo "$item --> $item.pcx"
+# Rename-Item -Path "project.txt" -NewName "d:\archive\old-project.txt"
+Rename-Item $item -NewName "$item.pcx"
+}
+
+sleep 5
